6221959771998ac91c46ead4fcba1c96875f4e02
Bora Poçan <60720346+borapocan@users.noreply.github.com>
2020-10-01 21:51:12 +0300

    Diagonal Difference

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace DiagonalDifference
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            int sumPrimaryDiagonal = 0;
+
+            int sumSecondaryDiagonal = 0;
+
+            int n = int.Parse(ReadLine());
+
+            for (int i = 0, j = n - 1; i < n; i++, j--)
+            {
+                var a_temp = ReadLine().Split(' ');
+
+                var newRow = Array.ConvertAll(a_temp, int.Parse);
+
+                sumPrimaryDiagonal += newRow[i];
+
+                sumSecondaryDiagonal += newRow[j];
+            }
+
+            WriteLine(Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal));
+        }
+    }
+}
